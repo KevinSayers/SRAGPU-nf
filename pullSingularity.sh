bfb5863d4a043f4fc19bcc6caf72e407c753790c
KevinSayers <sayerskt@gmail.com>
2017-08-24 15:29:19 +0200

    added in the Singularity pull bash script

---
+++ pullSingularity.sh
@@ -0,0 +1,5 @@
+singularity pull shub://KevinSayers/BarraCUDA_Singularity --name KevinSayers-BarraCUDA_Singularity.img
+singularity pull docker://sayerskt/sratoolkit --name sayerskt-sratoolkit.img
+singularity pull docker://sayerskt/samtools --name sayerskt-samtools.img
+singularity pull docker://sayerskt/opossum --name sayerskt-opossum.img
+singularity pull docker://sayerskt/platypus --name sayerskt-platypus.img
